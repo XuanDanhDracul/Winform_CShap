eefd0d61b91ef2c9080787c4287f6b90189ebe96
xuandanh <xuandanh321@gmail.com>
2021-01-08 10:19:35 +0700

    Create project

---
+++ ConsoleApp2/Event_Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckClock
+{
+    class Prince
+    {
+        public  event PrinceChangEventHandle PrinceChange;
+        public delegate void PrinceChangEventHandle(decimal oldchange, decimal newchange);
+        decimal Prince_1 = 11000;
+        public decimal Price_Men
+        {
+            set
+            {
+                if (Prince_1 == value) return;
+                var olChanger = value;
+                Prince_1 = value;
+                PrinceChange?.Invoke(olChanger, Prince_1);
+            }
+            get => Prince_1;
+        }
+       
+    }
+    class Event_Test
+    {
+        public static void PrinceChange_Aument(decimal oldchange, decimal newchange)
+        {
+            Console.WriteLine($"Gia xang tang {newchange }");
+        }
+        public static void Prince_Abatement(decimal oldchange, decimal newchange)
+        {
+            Console.WriteLine($"Gia xang giam {oldchange - newchange}");
+        }
+        public static void Main(string[] args)
+        {
+            var prince = new Prince();
+            prince.PrinceChange += PrinceChange_Aument;
+            prince.Price_Men = 20000;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+
+        public static long DateTimeToUnix(DateTime MyDateTime)
+        {
+            TimeSpan currentOffset = TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now);
+            TimeSpan timeSpan = MyDateTime - new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return (long)timeSpan.TotalSeconds;
+        }
+        public static void LocalTime()
+        {
+            //TimeSpan currentOffset = TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now);
+            long timestamp = DateTimeToUnix(DateTime.Now);
+            System.DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+            dateTime = dateTime.AddSeconds(timestamp);
+           // DateTime dateTime1 = dateTime.AddHours(+currentOffset.Hours);
+            Console.WriteLine(dateTime);
+        }
+        //public static void Main(string[] args)
+        //{
+        //    LocalTime();
+        //    Console.WriteLine(DateTimeToUnix(DateTime.Now));
+        //    Console.ReadKey();
+        //}
+    }
+}
